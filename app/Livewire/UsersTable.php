9e671501ae53edff666e6c331744753c245ff5b9
kaviranga <kaviranga5955@gmail.com>
2025-10-03 11:53:18 +0530

    app/Livewire/UsersTable.php

--- app/Livewire/UsersTable.php
+++ app/Livewire/UsersTable.php
@@ -3,11 +3,39 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\User;
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
 
-class UsersTable extends Component
+// class UsersTable extends Component
+// {
+//     public function render()
+//     {
+//         return view('livewire.users-table');
+//     }
+// }
+
+class UsersTable extends DataTableComponent
 {
-    public function render()
+    protected $model = User::class;
+
+    public function configure(): void
     {
-        return view('livewire.users-table');
+        $this->setPrimaryKey('id');
     }
+
+    public function columns(): array
+    {
+        return [
+            Column::make('ID', 'id')
+                ->sortable(),
+            Column::make('Name', 'name')
+                ->sortable()
+                ->searchable(),
+            Column::make('Email', 'email')
+                ->sortable()
+                ->searchable(),
+        ];
+    }
+
 }
