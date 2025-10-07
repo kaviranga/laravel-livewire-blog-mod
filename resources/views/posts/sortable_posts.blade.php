50afd4c9ae605b0b72d062bc372d6df00f06cd1c
kaviranga <kaviranga5955@gmail.com>
2025-10-07 16:05:53 +0530

    added sortable_posts.blade.php

--- resources/views/posts/sortable_posts.blade.php
+++ resources/views/posts/sortable_posts.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Sortable Users Table') }}
+            {{ __('Sortable Posts Table') }}
         </h2>
     </x-slot>
 
